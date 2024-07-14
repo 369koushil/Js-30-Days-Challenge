938a46b7e4820d4289c6c2d4289093d3ec93aaaf
koushil <koushil463@gmail.com>
2024-07-14 11:00:12 +0530

    adding daytwo

---
+++ Day 02/Day02.js
@@ -0,0 +1,10 @@
+const sum=(a,b)=>{return a+b;}
+const subtract=(a,b)=>{return a-b;}
+const mul=()=>{return a*b;}
+const div=(a,b)=>{return a/b;}
+const rem=(a,b)=>{return a%b}
+const power=(a,b)=>{return Math.pow(a,b)}
+
+console.log(power(2,4))
+
+
