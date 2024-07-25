41d2f68308b8d698b1e6cc2baac8d6e9878a8bc8
koushil <koushil463@gmail.com>
2024-07-25 15:58:10 +0530

    added day12

--- Day 12/Day12.js
+++ Day 12/Day12.js
@@ -1,10 +1,34 @@
+
+function div(a,b){
+    if(b==0){
+        throw new Error('we can tdivide with zero')
+    }
+    return a/b
+}
 try{
-    divison(0,0)
-}
-catch{
-    console.log('0/0 not divsible')
-}
-function divison(a,b){
-    console.log(a/b)
+    div(9,0)
+}catch(err){
+console.log(err)
 }
 
+class ApiKeyError extends Error {
+    constructor(msg) {
+      super(msg);
+    }
+  }
+  
+  function promise1() {
+    const pass = prompt('Enter passcode');
+    return new Promise((resolve, reject) => {
+      if (pass == 123) {
+        resolve('Welcome');
+      } else {
+        reject(new ApiKeyError("Invalid passcode"));
+      }
+    });
+  }
+  
+  promise1()
+    .then(r => console.log(r))
+    .catch(err => console.log(err));
+  
